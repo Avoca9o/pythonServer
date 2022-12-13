7335349455a333359cf07f8f2c1376da7f830358
Avoca9o <90473026+Avoca9o@users.noreply.github.com>
2022-12-13 22:43:20 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello, {name}"}
+
+@app.get("/ls/")
+async def ls():
+    output = list()
+    for root, dirs, files in os.walk("files"):
+        for filename in files:
+            output.append(filename)
+    if len(output) == 0:
+        return {"message": "database is empty"}
+    return {"files": output}
+
+@app.get("/download/{file}")
+async def download(file: str):
+    location = os.getcwd() + "/files/" + file
+    if os.path.exists(location):
+        return FileResponse(path=location, media_type='application/octet-stream', filename=file)
+    return {"message": "file not found"}
+
+@app.post("/upload/")
+async def upload(file: UploadFile = File(...)):
+    try:
+        for root, dirs, files in os.walk("files"):
+            for filename in files:
+                if filename == file.filename:
+                    return {"message": "file is already in database"}
+        content = file.file.read()
+        with open("files/" + file.filename, 'wb') as f:
+            f.write(content)
+    except Exception:
+        return {"message": "cannot upload file"}
+    finally:
+        file.file.close()
+    return {"statusCode": "OK"}
